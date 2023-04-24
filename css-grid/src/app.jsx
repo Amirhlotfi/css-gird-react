a21a5fbb82ad583fbf91fc232a97df832f02e1dc
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-04-24 12:58:22 +0200

    intial commit. Five box items placed in a css-grid.

---
+++ css-grid/src/app.jsx
@@ -0,0 +1,27 @@
+import "./app.css"
+
+const Box = ({ header, content }) => {
+  return (
+    <div className="box">
+      <h2> {header}</h2>
+      <p> {content}</p>
+    </div>
+  );
+};
+
+export function App() {
+  const content = "Lorem ipsum dolor sit amet, duo et nominavi mediocritatem. Putent audiam est ex."
+  const header = (char) => `Box ${char}`;
+  return (
+    <>
+      <h1 className="page-header">{"css grid"}</h1>
+      <div className="container">
+        <Box header={header("A")} content={content}/>
+        <Box header={header("B")} content={content}/>
+        <Box header={header("C")} content={content}/>
+        <Box header={header("D")} content={content}/>
+        <Box header={header("D")} content={content}/>
+      </div>
+    </>
+  );
+}
