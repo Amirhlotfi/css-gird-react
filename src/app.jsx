763e17bcf61303d52dfb50eab52ba340674a2514
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-04-25 12:16:01 +0200

    Formated

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
-import "./app.css"
-const content = "Lorem ipsum dolor sit amet, duo et nominavi mediocritatem. Putent audiam est ex."
-
+import "./app.css";
+const content =
+  "Lorem ipsum dolor sit amet, duo et nominavi mediocritatem. Putent audiam est ex.";
 
 const Box = ({ header, content }) => {
   return (
@@ -14,13 +14,12 @@ const Box = ({ header, content }) => {
 const ThreeBoxes = () => {
   return (
     <>
-        <Box header={"Box A1 "} content={content}/>
-        <Box header={"Box A2 "} content={content}/>
-        <Box header={"Box A3 "} content={content}/>
-
+      <Box header={"Box A1 "} content={content} />
+      <Box header={"Box A2 "} content={content} />
+      <Box header={"Box A3 "} content={content} />
     </>
-  )
-}
+  );
+};
 
 export function App() {
   const header = (char) => `Box ${char}`;
@@ -29,10 +28,10 @@ export function App() {
       <h1 className="page-header">{"css grid"}</h1>
       <div className="container">
         <ThreeBoxes />
-        <Box header={header("B")} content={content}/>
-        <Box header={header("C")} content={content}/>
-        <Box header={header("D")} content={content}/>
-        <Box header={header("E")} content={content}/>
+        <Box header={header("B")} content={content} />
+        <Box header={header("C")} content={content} />
+        <Box header={header("D")} content={content} />
+        <Box header={header("E")} content={content} />
       </div>
     </>
   );
